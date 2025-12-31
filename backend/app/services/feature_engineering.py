14ec480357c47f8e4963652d23d7b3989a70e280
Deju Adejo <ojomideju2003@gmail.com>
2025-12-31 06:00:26 +0100

    chore(features): add todos for probe

--- backend/app/services/feature_engineering.py
+++ backend/app/services/feature_engineering.py
@@ -82,6 +82,7 @@ class FeatureEngineeringService:
 
         # -- Geolocation features
         geolocation_distance = context_data.get('geolocation_distance_km', 0)
+        # TODO: To be recalculated by geolocation distance once computed
         is_geolocation_anomaly = context_data.get('is_geolocation_anomaly', False)
 
         if geolocation_distance > 500:
@@ -122,7 +123,7 @@ class FeatureEngineeringService:
 
         features['session_duration_seconds'] = session_duration
         features['form_fill_time_minutes'] = form_fill_time / 60.0
-        # TODO: Probe rushed form logic. Proposing less than a minute
+        # TODO: Probe rushed form logic & suspicious session. Proposing less than a minute
         features['is_rushed_form'] = 1 if form_fill_time < 180 else 0
         features['is_suspicious_session'] = 1 if (form_fill_time < 120 or form_fill_time > 1200) else 0
         features['pages_visited'] = pages_visited
